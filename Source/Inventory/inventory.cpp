21a3bb9af8ec9a8530b3cf2497479e4011252837
Aycy03 <justinmeng255@gmail.com>
2025-06-08 19:11:37 +0200

    added functions to be implemented

--- Source/Inventory/inventory.cpp
+++ Source/Inventory/inventory.cpp
@@ -1,1 +1,36 @@
 #include "inventory.h"
+
+// implementations for Inventory class
+
+Inventory::Inventory()
+{
+    for (unsigned int i = 0; i < MAX_INV_SLOTS; i++)
+    {
+        m_slots[i] = InvSlot();
+    }
+}
+
+/* Inventory::~Inventory()
+{
+
+} */
+
+void Inventory::draw(RenderMaster &renderer)
+{
+
+}
+
+void Inventory::update()
+{
+
+}
+
+void mouseInput()
+{
+    
+}
+
+void Inventory::toggleVisibility()
+{
+    m_showInventory = !m_showInventory;
+}
