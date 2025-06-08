d331da7aca8bf6b0872f2e7d7969a2b88d675736
Aycy03 <justinmeng255@gmail.com>
2025-06-08 18:39:49 +0200

    added more variables and methods

--- Source/Inventory/inventory.h
+++ Source/Inventory/inventory.h
@@ -2,6 +2,7 @@
 #define INVENTORY_H_INCLUDED
 
 #define MAX_INV_SLOTS 36
+#define MAX_STACK_SIZE 64
 
 #include <SFML/Graphics.hpp>
 #include <Item/ItemStack.h>
@@ -19,6 +20,8 @@ class Inventory
 
         void draw(RenderMaster &renderer);
 
+        void changeVisibility();
+
     private:
 
         std::vector<InvSlot> m_slots;
@@ -31,9 +34,17 @@ class InvSlot
 {
     public:
 
+        InvSlot();
+
+        ~InvSlot();
+
+        void addItem(const Material &material);
+
     private:
 
         ItemStack m_items;
+
+        u_int m_maxStack = MAX_STACK_SIZE;
 };
 
 #endif // INVENTORY_H_INCLUDED
